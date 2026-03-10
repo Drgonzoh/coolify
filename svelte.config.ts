4aa06472247e6934a414ff4fd08b054873b461e1
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-10 14:13:45 +0100

    chore(setup): enforce svelte 5 runes mode globally

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,23 +1,22 @@
-import { vitePreprocess, type SvelteConfig } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess, type SvelteConfig } from "@sveltejs/vite-plugin-svelte";
 
 const config: SvelteConfig = {
 	preprocess: vitePreprocess(),
 	compilerOptions: {
-		// TODO: Enforce runes mode on all files once Inertia v3 https://github.com/inertiajs/inertia/tree/3.x is released
-		// runes: true,
+		runes: true,
 		discloseVersion: false,
 		modernAst: true,
 		dev: true,
 		experimental: {
 			async: true,
-		}
+		},
 	},
 	vitePlugin: {
 		inspector: {
-			showToggleButton: 'always',
-			toggleButtonPos: 'top-right'
-		}
-	}
+			showToggleButton: "always",
+			toggleButtonPos: "top-right",
+		},
+	},
 };
 
 export default config;
