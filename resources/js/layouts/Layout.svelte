d1e410fddb618ab83e5fc850bca969038c15a560
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-10 14:41:41 +0100

    chore(ui): add persistent layout with page title

---
+++ resources/js/layouts/Layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { useLayoutProps, page } from "@inertiajs/svelte";
+
+	const layout = useLayoutProps({
+		title: "",
+	});
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>{$layout.title ? `${$layout.title} - ` : ""}{page.props.appName}</title>
+</svelte:head>
+
+<main>
+	{@render children()}
+</main>
