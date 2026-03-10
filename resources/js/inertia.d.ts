5ed3b2e498f37032f266377907ccce1963d6ee73
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-03-10 23:16:23 +0100

    fix(types): inertia type declaration and shared props typing
    - when there is no import or export statement on a .d.ts file ts treats it as a "script" instead of a "module". In a "script", declare module "X" is an ambient module declaration that replaces the module. Adding any import or empty export makes the file a module, which changes declare module "X" to a module augmentation that merges with the real module. See: https://www.typescriptlang.org/docs/handbook/modules/reference.html#ambient-modules

---
+++ resources/js/inertia.d.ts
@@ -0,0 +1,13 @@
+import "@inertiajs/core";
+
+declare module "@inertiajs/core" {
+	export interface InertiaConfig {
+		sharedPageProps: {
+			appName: string;
+		};
+		// flashDataType: {
+		//     toast?: { type: "success" | "error"; message: string };
+		// };
+		errorValueType: string[];
+	}
+}
